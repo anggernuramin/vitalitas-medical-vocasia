777b31144c4448405e1e4dd34f6857c1371a414c
anggernuramin <anggern514@gmail.com>
2023-11-29 05:11:03 +0700

    improvement: setup tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    container: {
+      center: true,
+    },
+
+    extend: {
+      colors: {
+        color: {
+          primary90:'#B97375',
+          primary30: '#F0DEDF1A',
+          primary10: '#F0DEDF',
+          black: '#003D87',
+          overlay: '#121619',
+          coolGray10: '#F2F4F8',
+          coolGray20: '#DDE1E6',
+          coolGray30: '#C1C7CD',
+          coolGray40: '#A2A9B0',
+          coolGray50: '#878d96',
+          coolGray60: '#697077',
+          coolGray70: '#4D5358',
+          coolGray80: '#343A3F',
+          coolGray90: '#21272A',
+          coolGray100: '#121619',
+        }
+      }
+    },
+  },
+  plugins: [daisyui],
+};
